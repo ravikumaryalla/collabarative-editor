0d19c0c94e3f4aa7abd17110b5704db0406330c2
ravi kumar <ravikumaryalla3@gmail.com>
2025-10-23 18:18:15 +0530

    implemented convex  for database

---
+++ convex/schema.ts
@@ -0,0 +1,17 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+export default defineSchema({
+  documents: defineTable({
+    title: v.string(),
+    initialContent: v.optional(v.string()),
+    ownerId: v.string(),
+    roomId: v.optional(v.string()),
+    organizationId: v.optional(v.string()),
+  })
+    .index("by_owner", ["ownerId"])
+    .index("by_organization", ["organizationId"])
+    .searchIndex("search_title", {
+      searchField: "title",
+      filterFields: ["organizationId", "ownerId"],
+    }),
+});
