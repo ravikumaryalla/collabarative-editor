7e94d400ad1e6c7c5ba7eb130a1db9cedc9876d9
ravi kumar <ravikumaryalla3@gmail.com>
2025-10-27 21:12:09 +0530

    authored client provider for convex

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,6 +8,8 @@
  * @module
  */
 
+import type * as document from "../document.js";
+
 import type {
   ApiFromModules,
   FilterApi,
@@ -22,7 +24,9 @@ import type {
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  document: typeof document;
+}>;
 declare const fullApiWithMounts: typeof fullApi;
 
 export declare const api: FilterApi<
--- src/components/ConvexClientprovider.tsx
+++ src/components/ConvexClientprovider.tsx
@@ -1,10 +1,35 @@
 "use client";
 
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import {
+  ConvexReactClient,
+  AuthLoading,
+  Authenticated,
+  Unauthenticated,
+} from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ClerkProvider, useAuth, SignIn } from "@clerk/clerk-react";
+
 import { ReactNode } from "react";
+import FullScreenLoader from "./FullScreenLoader";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <Authenticated>{children}</Authenticated>
+        <Unauthenticated>
+          <div className="w-screen  h-screen flex flex-col justify-center items-center">
+            <SignIn />
+          </div>
+        </Unauthenticated>
+        <AuthLoading>
+          <FullScreenLoader label="Loading . . ." />
+        </AuthLoading>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 }
